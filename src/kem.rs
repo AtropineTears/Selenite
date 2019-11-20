1ee27b2f2bfcb5257418af4763ac3d7db3bc6a1a
0xSilene <0xsilene@MacBook.local>
2019-11-20 12:51:32 -0500

    Commit

--- src/kem.rs
+++ src/kem.rs
@@ -1,6 +1,9 @@
 use pqcrypto_newhope::newhope1024cca::*;
 use pqcrypto_traits::kem::{PublicKey,SecretKey,SharedSecret,Ciphertext};
-use slice_as_array;
+use base64;
+
+// Public-Key: 1,824
+// Shared-Secret: 32
 
 pub struct NewHopeInitiater {
     public_key: Vec<u8>,
@@ -11,24 +14,26 @@ impl NewHopeInitiater {
 
         let (pk,sk) = keypair();
 
-        pk.as_bytes().to_vec()
-        sk.as_bytes().to_vec();
+        let pk_vec = pk.as_bytes().to_vec();
+        let sk_vec = sk.as_bytes().to_vec();
 
         NewHopeInitiater {
-            public_key: pk_array,
-            secret_key: sk_array,
+            public_key: pk_vec,
+            secret_key: sk_vec,
         }
 
 
     }
-    fn send(&self) -> [u8;1_824] {
+    fn send(&self) -> Vec<u8> {
         return self.public_key
     }
+    // Ciphertext of 2,208 Bytes
     fn recieve(&self, ct: [u8;2_208]) ->  {
         let x = decapsulate(&Ciphertext::from_bytes(&ct).unwrap(), &SecretKey::from_bytes(&self.secret_key).unwrap());
         return x.as_bytes()
     }
 }
+
 pub struct NewHopeReceiver {
     public_key: [u8;1_824],
     shared_secret: [u8;32],
---
+++ src/pivot.rs
@@ -0,0 +1,20 @@
+pub struct Pivot {
+    crs: String,
+}
+
+pub struct SphincsBlock {
+    id: u64,
+    cluster: u64,
+    nonce: u64,
+    sphincs_pk: String,
+}
+
+impl Pivot {
+    pub fn new() -> Self {
+
+    }
+    pub fn connect(){
+
+    }
+}
+
